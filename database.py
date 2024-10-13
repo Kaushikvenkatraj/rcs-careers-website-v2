2a262b43c82282f8be494b03d3a7acdec3e67156
unknown <kaushik.venkat17@gmail.com>
2024-10-13 23:56:24 +0530

    Addded connection to DB by post method

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
 import os 
 
-engine = create_engine("DB_COnnection-String")
+load_dotenv()
+
+db_connection_string= os.getenv("DB_CONNECTION_STRING")
+
+engine = create_engine(db_connection_string)
 
 def load_jobs_from_db():
   with engine.connect() as conn:
@@ -19,4 +24,17 @@ def load_job_from_db(id):
        return None
     else:
        return dict(rows[0]._mapping)
+     
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedn_url, education, work_exp, resume_url) VALUES (:job_id, :full_name, :email, :linkedn_url, :education, :work_exp, :resume_url)")
+    
+    conn.execute(query, 
+                 job_id=job_id, 
+                 full_name=data['full_name'],
+                 email=data['email'],
+                 education=data['education'],
+                 work_exp=data['work_exp'],
+                 resume_url=data['resume_url']
+                 )
 
