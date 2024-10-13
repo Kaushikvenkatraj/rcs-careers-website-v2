1f852cc55680aa6692aebce5ff51d7cd200f9364
unknown <kaushik.venkat17@gmail.com>
2024-10-13 15:11:50 +0530

    Added job page and linked bootstrap to other pages

--- database.py
+++ database.py
@@ -1,9 +1,7 @@
 from sqlalchemy import create_engine, text
 import os 
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
-
-engine = create_engine(db_connection_string)
+engine = create_engine("mysql+pymysql://kalki:KalkiLoading2025@database-1.c25fp4vtulci.us-east-1.rds.amazonaws.com/rcscareers?charset=utf8mb4")
 
 def load_jobs_from_db():
   with engine.connect() as conn:
@@ -13,6 +11,12 @@ def load_jobs_from_db():
       jobs.append(dict(row._mapping))
     return jobs
 
-  
-
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id = {id}"))
+    rows = result.all()
+    if len(rows) == 0:
+       return None
+    else:
+       return dict(rows[0]._mapping)
 
